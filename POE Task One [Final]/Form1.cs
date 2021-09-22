555238e5784d55a21a449ddbde44b27de629353c
VinnieRepo <91118842+VinnieRepo@users.noreply.github.com>
2021-09-22 10:00:03 +0200

    Add project files.

---
+++ POE Task One [Final]/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POE_Task_One__Final_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    abstract class tile
+
+
+    {
+        protected int Tilex;
+        protected int Tiley;
+
+        public void valuex(int value)
+        {
+            Tilex = value;
+        }
+        public void valuey(int value)
+        {
+            Tiley = value;
+        }
+        public int TileType;
+        public class defaults : tile
+        {
+            public void Basevalues(int xval, int yval, int tiletype)
+            {
+                Tilex = xval;
+                Tiley = yval;
+                TileType = tiletype;
+
+
+
+            }
+
+        }
+
+        public class obstacles : defaults
+        {
+            public void Places(int xval, int yval, int tiletype)
+            {
+                Basevalues(xval, yval, tiletype);
+            }
+
+
+
+        }
+        public class emptyTiles : defaults
+        {
+            public void Places(int xval, int yval, int tiletype)
+            {
+                Basevalues(xval, yval, tiletype);
+            }
+
+
+        }
+    }
+}
